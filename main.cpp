f10ed2f4fdef6911c688298f5e6cbbfa5932e6f5
ex1st0r <sanzeting@mail.ru>
2014-12-16 02:50:45 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,48 @@
 #include <iostream>
+#include <fstream>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
-//Искомая функция
-double J(double x){
+double func(double x){
     return x*x+2*x;
 }
 
-//Метод золотого сечения
-double *zolot(double a, double b, double eps){
-    double delta=eps/2,u1,u2,min_x;
-    static double res[2];
+double *gold_ratio(double (*J)(double),double a, double b, double eps){
+    double delta=eps/2,u1,u2,min_x,J1,J2;
+    double *res = new double[2];
     int i=0;
+
+    u1 = a + (3-sqrt(5))*(b-a)/2;
+    u2 = a + (sqrt(5) - 1)*(b-a)/2;
+    J1 = J(u1);
+    J2 = J(u2);
+
     while (fabs(b-a) > eps){
-        u1 = a + (3-sqrt(5))*(b-a)/2;
-        u2 = a + (sqrt(5) - 1)*(b-a)/2;
-        if (J(u1) <= J(u2)){
+        if (J1 <= J2){
             b = u2;
-            min_x = u1;
+            u2 = u1;
+            J2 = J1;
+            u1 = a + (3 - sqrt(5))*(b-a)/2;
+            J1 = J(u1);
         } else {
             a = u1;
-            min_x = u2;
+            u1 = u2;
+            J1 = J2;
+            u2 = a + (sqrt(5) - 1)*(b-a)/2;
+            J2 = J(u2);
         }
         i++;
     };
     res[0] = i;
-    res[1] = min_x;
+    res[1] = (u1+u2)/2;
     return res;
 }
 
-//Метод дихотомии
-double *dychotomy(double a, double b, double eps){
+double *dychotomy(double (*J)(double), double a, double b, double eps){
     double delta=eps/2,u1,u2, min_x;
-    static double res[2];
+    double *res = new double[2];
     int i=0;
     while (fabs(b - a) > eps){
         u1 = (a+b-delta)/2;
@@ -47,23 +56,22 @@ double *dychotomy(double a, double b, double eps){
         };
         i++;
     };
-    res[0] = i;
+    res[0] = i * 2; //Количество вычислений функций
     res[1] = min_x;
     return res;
 }
 
 int main()
 {
-    double a = -5, b = 5, eps = 1e-10;
+    double a = -7, b = 4, eps = 1e-10;
     double *arr;
-	
-    printf("eps=%g\r\n[%g;%g]\r\n\r\n", eps, a, b);
 
-    arr = zolot(a, b, eps);
+    arr = gold_ratio(func, a, b, eps);
     printf("zolot\r\ni=%g;min_x=%g\n", arr[0], arr[1]);
 
-    arr = dychotomy(a, b, eps);
+    arr = dychotomy(func, a, b, eps);
     printf("\r\ndychotomy\r\ni=%g;min_x=%g\n", arr[0], arr[1]);
+
 
     return 0;
 }
